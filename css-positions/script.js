f409c82a8bbeddee8cf97ffdd37f98d0fad3695e
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-11-12 11:01:34 -0800

    (feat) add css positioning example

---
+++ css-positions/script.js
@@ -0,0 +1,120 @@
+/* global $ */
+function purpleToStatic () {
+  $('.purple').css('position', 'static');
+  $('.purple>h4').html('Position is static');
+}
+
+function purpleToRelative () {
+  $('.purple').css('position', 'relative');
+  $('.purple>h4').html('Position is relative');
+}
+
+function purpleToAbsolute () {
+  $('.purple').css('position', 'absolute');
+  $('.purple>h4').html('Position is absolute');
+}
+
+function purpleToFixed () {
+  $('.purple').css('position', 'fixed');
+  $('.purple>h4').html('Position is fixed');
+}
+
+function blueToStatic () {
+  $('.blue').css('position', 'static');
+  $('.blue>h4').html('Position is static');
+}
+
+function blueToRelative () {
+  $('.blue').css('position', 'relative');
+  $('.blue>h4').html('Position is relative');
+}
+
+function blueToAbsolute () {
+  $('.blue').css('position', 'absolute');
+  $('.blue>h4').html('Position is absolute');
+}
+
+function blueToFixed () {
+  $('.blue').css('position', 'fixed');
+  $('.blue>h4').html('Position is fixed');
+}
+
+function yellowToStatic () {
+  $('.yellow').css('position', 'static');
+  $('.yellow>h4').html('Position is static');
+}
+
+function yellowToRelative () {
+  $('.yellow').css('position', 'relative');
+  $('.yellow>h4').html('Position is relative');
+}
+
+function yellowToAbsolute () {
+  $('.yellow').css('position', 'absolute');
+  $('.yellow>h4').html('Position is absolute');
+}
+
+function yellowToFixed () {
+  $('.yellow').css('position', 'fixed');
+  $('.yellow>h4').html('Position is fixed');
+}
+
+function orange1ToStatic () {
+  $('.orange1').css('position', 'static');
+  $('.orange1>h4').html('Position is static');
+}
+
+function orange1ToRelative () {
+  $('.orange1').css('position', 'relative');
+  $('.orange1>h4').html('Position is relative');
+}
+
+function orange1ToAbsolute () {
+  $('.orange1').css('position', 'absolute');
+  $('.orange1>h4').html('Position is absolute');
+}
+
+function orange1ToFixed () {
+  $('.orange1').css('position', 'fixed');
+  $('.orange1>h4').html('Position is fixed');
+}
+
+function orange2ToStatic () {
+  $('.orange2').css('position', 'static');
+  $('.orange2>h4').html('Position is static');
+}
+
+function orange2ToRelative () {
+  $('.orange2').css('position', 'relative');
+  $('.orange2>h4').html('Position is relative');
+}
+
+function orange2ToAbsolute () {
+  $('.orange2').css('position', 'absolute');
+  $('.orange2>h4').html('Position is absolute');
+}
+
+function orange2ToFixed () {
+  $('.orange2').css('position', 'fixed');
+  $('.orange2>h4').html('Position is fixed');
+}
+
+function pinkToStatic () {
+  $('.pink').css('position', 'static');
+  $('.pink>h4').html('Position is static');
+}
+
+function pinkToRelative () {
+  $('.pink').css('position', 'relative');
+  $('.pink>h4').html('Position is relative');
+}
+
+function pinkToAbsolute () {
+  $('.pink').css('position', 'absolute');
+  $('.pink>h4').html('Position is absolute');
+}
+
+function pinkToFixed () {
+  $('.pink').css('position', 'fixed');
+  $('.pink>h4').html('Position is fixed');
+}
